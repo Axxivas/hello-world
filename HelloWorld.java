3033f02aedfbf837a47e9cee7a72cec26baea678
Bryan Rivas <rivas.bryan00@yahoo.com>
2021-03-15 12:59:53 -0500

    Added 12 lines. Prints "Hello World! Git is a cool an handy tool for coding!

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!\n"
+				+ "Git is a cool and handy tool for coding!");
+
+	}
+
+}
