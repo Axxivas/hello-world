f8f984c48497df095aeb4493155a2eb620db20ff
Axxivas <77760405+Axxivas@users.noreply.github.com>
2021-03-15 12:49:42 -0500

    Add files via upload

---
+++ src/hello_world/Hello_git.java
@@ -0,0 +1,13 @@
+package hello_world;
+
+public class Hello_git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("Git and Github is a nice "
+				+ "handy tool!");
+
+	}
+
+}
